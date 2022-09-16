796f9469199b44b3b3a118849650e03df588f873
naily26 <nailymasruri@gmail.com>
2022-09-16 21:07:49 +0700

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'uploadfile.dart';
+import 'videoplayer.dart';
+
+// void main() => runApp(MaterialApp(
+//       home: UploadFile(),
+//       debugShowCheckedModeBanner: false,
+//     ));
+
+void main() => runApp(const VideoPlayerApp());
+
+
